7cf205995a45634f23e019ca5f9ac7084cb2e431
Racocon <diyanah.nadzree@live.com>
2019-06-18 14:35:15 +0800

    minor edit

--- src/views/customer/components/KYC.vue
+++ src/views/customer/components/KYC.vue
@@ -1,48 +1,55 @@
 <template>
-  <el-table
-    :data="tableData.filter(data => !search || data.name.toLowerCase().includes(search.toLowerCase()))"
-    style="width: 100%">
+  <div>
+    <h1> KYC History </h1>
 
-    <el-table-column
-      label="Title"
-      prop="title">
-    </el-table-column>
+    <div class="app-container">
+      <el-table
+        :data="tableData.filter(data => !search || data.user.toLowerCase().includes(search.toLowerCase()))"
+        style="width: 100%">
 
-    <el-table-column
-      label="Date"
-      prop="date">
-    </el-table-column>
+        <el-table-column
+          label="Title"
+          prop="title">
+        </el-table-column>
 
-    <el-table-column
-      label="User"
-      prop="user">
-    </el-table-column>
+        <el-table-column
+          label="Date"
+          prop="date">
+        </el-table-column>
 
-    <el-table-column
-      align="right">
-      <template slot="header" slot-scope="scope">
-        <el-input
-          v-model="search"
-          size="mini"
-          placeholder="Type to search"/>
-      </template>
-      <template slot-scope="scope">
-          
-        <!-- Download Excel button -->
-        <!-- <el-button
-          size="mini"
-          type="success"
-          @click="handleExcel(scope.$index, scope.row)">Excel</el-button> -->
-        
-        <!-- Download PDF button -->
-        <el-button
-          size="mini"
-          type="primary"
-          @click="handlePDF(scope.$index, scope.row)">PDF</el-button>
-      </template>
-    </el-table-column>
+        <el-table-column
+          label="User"
+          prop="user">
+        </el-table-column>
 
-  </el-table>
+        <el-table-column
+          align="right">
+          <!-- Search bar -->
+          <template slot="header" slot-scope="scope">
+            <el-input
+              v-model="search"
+              size="mini"
+              placeholder="Type to search"/>
+          </template>
+
+          <template slot-scope="scope">
+            <!-- Download Excel button -->
+            <el-button
+              size="mini"
+              type="success"
+              @click="handleExcel(scope.$index, scope.row)">Excel</el-button>
+            
+            <!-- Download PDF button -->
+            <el-button
+              size="mini"
+              type="primary"
+              @click="handlePDF()">PDF</el-button>
+          </template>
+        </el-table-column>
+
+      </el-table> 
+    </div>
+  </div>
 </template>
 
 <script>
@@ -54,7 +61,7 @@
 
     data() {
       return {
-        name: '',
+        pdfname: '',
         tableData: [{
           title: 'KYC 2019.05',
           date: '03/05/2019',
@@ -85,7 +92,7 @@
       handlePDF() {
         let pdfName = 'test'; 
         var doc = new jspdf();
-        doc.text(this.name, 10, 10);
+        doc.text(this.pdfname, 10, 10);
         doc.save(pdfName + '.pdf');
       }
     },
