17edc35366bb7a3980f0092340baabfb6dcc7a81
Lim Zi Hao <lzhao1880@gmail.com>
2019-06-30 03:18:23 +0800

    added Actions

--- src/views/question/index.vue
+++ src/views/question/index.vue
@@ -5,35 +5,67 @@
         <el-button
           style="float: right"
           type="primary"
-          @click= "navigateTo({name: 'questionCreate'})"
+          @click="navigateTo({name: 'questionCreate'})"
           icon="el-icon-plus"
         ></el-button>
       </el-col>
     </el-row>
     <el-row>
-      <el-table 
-        @row-click= "navigateTo({name:'questionCreate'})"
-        v-loading= "listLoading"
+      <el-table
+        
+        v-loading="listLoading"
         :data="user.filter(data => !search || data.name.toLowerCase().includes(search.toLowerCase()))"
         element-loading-text="Loading"
-        border
         fit
         highlight-current-row
       >
-        <el-table-column label="Question Name" width=auto >
-          <template slot-scope="scope">{{ scope.row.display_text }}</template>
+        <el-table-column label="Question Name" width="auto" >
+          <template
+            slot-scope="scope"
+            @click="navigateTo ('questionCreate')"
+          >{{ scope.row.display_text }}</template>
         </el-table-column>
-        <el-table-column label="Section" width="80">
+        <el-table-column label="Section" width="80" >
           <template slot-scope="scope">{{ scope.row.section }}</template>
         </el-table-column>
-        <el-table-column label="Origin" width="80">
-          <template slot-scope="scope">{{ scope.row.origin  }}</template>
+        <el-table-column label="Origin" width="80" >
+          <template slot-scope="scope">{{ scope.row.origin }}</template>
         </el-table-column>
-        <el-table-column label="Question Category" width="150">
+        <el-table-column label="Question Category" width="150" >
           <template slot-scope="scope">{{ scope.row.category.name }}</template>
         </el-table-column>
-        <el-table-column label="Question Type" width="150">
+        <el-table-column label="Question Type" width="150" >
           <template slot-scope="scope">{{ scope.row.type.name }}</template>
+        </el-table-column>
+        <el-table-column align="center" label="Action" width="150" >
+          <template align="center" slot-scope="scope" v-if="scope.row.origin!='epicor'">
+            <el-tooltip :open-delay="tooltipDelay" content="Profile" placement="top">
+              <el-button
+                size="mini"
+                icon="el-icon-search"
+                @click="navigateTo ({name: 'questionView', params:{questionId: scope.row.id}})"
+                circle
+              ></el-button>
+            </el-tooltip>
+            <el-tooltip :open-delay="tooltipDelay" content="Update" placement="top">
+              <el-button
+                size="mini"
+                type="primary"
+                icon="el-icon-edit"
+                @click="navigateTo ({name: 'userUpdate', params:{questionId: scope.row.id}})"
+                circle
+              ></el-button>
+            </el-tooltip>
+            <el-tooltip :open-delay="tooltipDelay" content="Delete" placement="top">
+              <el-button
+                size="mini"
+                type="danger"
+                icon="el-icon-delete"
+                @click="changeDeleteId(scope.row.id)"
+                circle
+              ></el-button>
+            </el-tooltip>
+          </template>
         </el-table-column>
       </el-table>
     </el-row>
@@ -57,14 +89,9 @@ import { getQuestionIndex } from "@/api/kyc/question";
 export default {
   data() {
     return {
-      //Dummy Data
       user: [],
       listLoading: false,
       tooltipDelay: 500,
-      status: 1,
-
-      idDelete: "",
-      dialogDelete: false,
 
       totalUserPage: 0,
       userQuery: {
