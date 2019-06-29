80f298108be6f93d728cf513d1bf5d838e3df092
Lim Zi Hao <lzhao1880@gmail.com>
2019-06-30 03:18:06 +0800

    added putQuestion and getQuestionShow

--- src/api/kyc/question.js
+++ src/api/kyc/question.js
@@ -1,4 +1,5 @@
 import request from '@/utils/request'
+import qs from 'qs'
 
 export function getQuestionIndex(query) {
   return request({
@@ -6,11 +7,43 @@ export function getQuestionIndex(query) {
     method: 'get'
   })
 }
-
-export function createQuestion(data) {
+export function getQuestionShow(userId) {
+  return request({
+    url: `/kyc/question/${userId}`,
+    method: 'get'
+  })
+}
+export function createQuestion(data) {  
   return request({
     url: '/kyc/question',
     method: 'post',
     data
+  })
+}
+export function putQuestion(question) {
+  return request({
+    url: `/kyc/question/${question.id}`,
+    method: 'put',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    data: qs.stringify({
+      question: {
+        field_name: question.field_name,
+        display_text: question.display_text,
+        section: question.section,
+        description: question.description,
+        origin: question.origin,
+        category_id: question.category_id,
+        type_id: question.type_id,
+        default_data: question.default_data,
+        only_default: question.only_default,
+        input_type: question.input_type,
+        fields: question.fields,
+        is_hidden: question.is_hidden,
+        is_mandatory: question.is_mandatory
+      }
+
+    })
   })
 }
