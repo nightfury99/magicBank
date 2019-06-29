74502912ac0af94d7f8cba6ba45f3c4080c8906b
Lim Zi Hao <lzhao1880@gmail.com>
2019-06-29 15:56:38 +0800

    Added QuestionList and Pagination

--- src/views/question/index.vue
+++ src/views/question/index.vue
@@ -1,78 +1,120 @@
 <template>
-    <div class="app-container">
-        <el-row>
-	        <el-col >
-			    <el-button style="float: right" type="warning" @click="navigateTo({name: 'attributeCreate'})" >+</el-button>
-            </el-col>
-	    </el-row>
-        <el-row>
-            <el-table :data ="attributes">
-                <el-table-column label="Attribute"  prop="name">			  
-                </el-table-column>
-            </el-table>   
-        </el-row>
-        <el-row>
-		    <el-col :span="24" align="center">
-			    <pagination
-				    background
-				    layout="prev, pager, next"
-				    :page-count="query.page_count"
-				    @pagination="attributes" />
-		    </el-col>
-	    </el-row>
+  <div class="app-container">
+    <el-row>
+      <el-col>
+        <el-button
+          style="float: right"
+          type="primary"
+          @click="navigateTo({name: 'questionCreate'})"
+          icon="el-icon-plus"
+        ></el-button>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-table
+        v-loading="listLoading"
+        :data="user.filter(data => !search || data.name.toLowerCase().includes(search.toLowerCase()))"
+        element-loading-text="Loading"
+        border
+        fit
+        highlight-current-row
+      >
+        <el-table-column label="Question Name" width=auto >
+          <template slot-scope="scope">{{ scope.row.display_text }}</template>
+        </el-table-column>
+        <el-table-column label="Section" width="80">
+          <template slot-scope="scope">{{ scope.row.section }}</template>
+        </el-table-column>
+        <el-table-column label="Origin" width="80">
+          <template slot-scope="scope">{{ scope.row.origin  }}</template>
+        </el-table-column>
+        <el-table-column label="Question Category" width="150">
+          <template slot-scope="scope">{{ scope.row.category.name }}</template>
+        </el-table-column>
+        <el-table-column label="Question Type" width="150">
+          <template slot-scope="scope">{{ scope.row.type.name }}</template>
+        </el-table-column>
+      </el-table>
+    </el-row>
 
-    
-    </div>
+    <el-row>
+      <el-col :span="24" align="center">
+        <pagination
+          background
+          layout="prev, pager, next"
+          :page-count="userQuery.page_count"
+          @pagination="questionList"
+        />
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
 <script>
-import Pagination from '@/components/Pagination'
+import Pagination from "@/components/Pagination";
+import { getQuestionIndex } from "@/api/kyc/question";
 export default {
-   
-    data(){
-        return{
-            //Dummy Data
-            attributes:[
-                {
-                    id: 1,
-                    name: "Test1"
-                },
-                {
-                    id: 1,
-                    name: "Test2"
-                },
-                {
-                    id: 1,
-                    name: "Test3"
-                }
-            ],
-            query: {
-				page: 1,
-				limit: 50,
-				page_count: 1
-			},   
-        }
+  data() {
+    return {
+      //Dummy Data
+      user: [],
+      listLoading: false,
+      tooltipDelay: 500,
+      status: 1,
+
+      idDelete: "",
+      dialogDelete: false,
+
+      totalUserPage: 0,
+      userQuery: {
+        page: 1,
+        limit: 50,
+        page_count: 1
+      }
+    };
+  },
+  components: { Pagination },
+  created() {
+    this.questionList();
+  },
+
+  methods: {
+    // navigate to specific route
+    navigateTo(route) {
+      this.$router.push(route);
     },
-   
+    async questionList(val) {
+      this.listLoading = true;
 
-    methods:{
-        // navigate to specific route
-        navigateTo(route){
-            this.$router.push(route)
-    },
+      console.log(val);
+      console.log(this.userQuery);
 
-       
+      if (val) {
+        this.userQuery.page = val.page;
+        console.log(this.userQuery);
+      }
 
-		
+      const meta = (await getQuestionIndex(this.userQuery)).data.meta
+        .pagination;
+
+      this.user = (await getQuestionIndex(this.userQuery)).data.data;
+
+      this.totalUserPage = meta.total;
+      this.userQuery.page_count = meta.total_pages;
+
+      console.log(this.user);
+
+      this.listLoading = false;
     }
-}
+  }
+};
 </script>
 
 <style scoped>
 .el-row {
-		margin-bottom: 20px;
-		&:last-child {
-		margin-bottom: 0;
-		}
-	}
+  margin-bottom: 20px;
+  &:last-child {
+    margin-bottom: 0;
+  }
+}
 </style>
