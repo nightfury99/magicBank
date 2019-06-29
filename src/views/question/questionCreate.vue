f263b30dc72a464a3787424a9770cc7f00a15c23
Lim Zi Hao <lzhao1880@gmail.com>
2019-06-30 00:56:40 +0800

    completed form, fully validated for empty input

--- src/views/question/questionCreate.vue
+++ src/views/question/questionCreate.vue
@@ -1,126 +1,169 @@
 <template>
   <div class="app-container">
-    <el-form label-position="top">
-      <el-form-item label="Question Name">
-        <el-col :span="24">
-          <el-input placeholder="Enter Question Name" v-model="newQuestion.display_text"/>
-        </el-col>
-      </el-form-item>
+    <el-card class="box-card">
+      <el-form label-position="top" :rules="rules" ref="newQuestion" :model="newQuestion">
+        <el-form-item label="Question Name" prop="display_text">
+          <el-col :span="24">
+            <el-input placeholder="Enter Question Name" v-model="newQuestion.display_text"/>
+          </el-col>
+        </el-form-item>
 
-      <el-form-item label="Section">
-        <el-select v-model="newQuestion.section" placeholder="Select Section">
-          <el-option
-            v-for="section in sectionOption"
-            :key="section.value"
-            :label="section.label"
-            :value="section.value"
-          ></el-option>
-        </el-select>
-      </el-form-item>
+        <el-row>
+          <el-col style="width:33%">
+            <el-form-item label="Section" prop="section">
+              <el-select v-model="newQuestion.section" placeholder="Select Section">
+                <el-option
+                  v-for="section in sectionOption"
+                  :key="section.value"
+                  :label="section.label"
+                  :value="section.value"
+                ></el-option>
+              </el-select>
+            </el-form-item>
+          </el-col>
+          <el-col style="width:33%">
+            <el-form-item label="Category" prop="category_id">
+              <el-select v-model="newQuestion.category_id" placeholder="Select Category">
+                <el-option
+                  v-for="(category,index) in categoryOption"
+                  :key="index"
+                  :label="category.name"
+                  :value="category.id"
+                ></el-option>
+              </el-select>
+            </el-form-item>
+          </el-col>
+          <el-col style="width:33%">
+            <el-form-item label="Type" prop="type_id">
+              <el-select v-model="newQuestion.type_id" placeholder="Select Type">
+                <el-option
+                  v-for="(type,index) in typeOption"
+                  :key="index"
+                  :label="type.name"
+                  :value="type.id"
+                ></el-option>
+              </el-select>
+            </el-form-item>
+          </el-col>
+        </el-row>
 
-      <el-form-item label="Question Description">
-        <el-input
-          type="textarea"
-          :rows="3"
-          placeholder="Enter Question Description"
-          v-model="newQuestion.description"
-        ></el-input>
-      </el-form-item>
+        <el-form-item label="Question Description" prop="description">
+          <el-input
+            type="textarea"
+            :rows="5"
+            placeholder="Enter Question Description"
+            v-model="newQuestion.description"
+          ></el-input>
+        </el-form-item>
 
-      <el-form-item label="Category">
-        <el-select v-model="newQuestion.category_id" placeholder="Select Category">
-          <el-option
-            v-for="(category,index) in categoryOption"
-            :key="index"
-            :label="category.name"
-            :value="category.id"
-          ></el-option>
-        </el-select>
-      </el-form-item>
-
-      <el-form-item label="Type">
-        <el-select v-model="newQuestion.type_id" placeholder="Select Type">
-          <el-option
-            v-for="(type,index) in typeOption"
-            :key="index"
-            :label="type.name"
-            :value="type.id"
-          ></el-option>
-        </el-select>
-      </el-form-item>
-
-      <el-form-item label="Question Input Type">
-        <el-select
-          v-model="newQuestion.input_type"
-          placeholder="Select Question Input Type"
-          v-on:change="onChange()"
+        <el-row>
+          <el-col style="width:33%">
+            <el-form-item label="Question Input Type" prop="input_type">
+              <el-select
+                v-model="newQuestion.input_type"
+                placeholder="Select Question Input Type"
+                v-on:change="onChange()"
+              >
+                <el-option
+                  v-for="questionType in questionTypeOption"
+                  :key="questionType.value"
+                  :label="questionType.label"
+                  :value="questionType.value"
+                ></el-option>
+              </el-select>
+            </el-form-item>
+          </el-col>
+          <el-col style="width:33%">
+            <el-form-item label="Hidden Question">
+              <el-switch v-model="newQuestion.is_hidden" active-text="Hidden"></el-switch>
+            </el-form-item>
+          </el-col>
+          <el-col style="width:33%">
+            <el-form-item label="Mandatory Question">
+              <el-switch v-model="newQuestion.is_mandatory" active-text="Mandatory"></el-switch>
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <div
+          v-if="newQuestion.input_type=='radio'||newQuestion.input_type=='checkbox'||newQuestion.input_type=='select'||newQuestion.input_type=='switch'"
         >
-          <el-option
-            v-for="questionType in questionTypeOption"
-            :key="questionType.value"
-            :label="questionType.label"
-            :value="questionType.value"
-          ></el-option>
-        </el-select>
-      </el-form-item>
-
-      <el-form-item
-        label="Question Choice"
-        v-if="newQuestion.questionType=='radio'||newQuestion.questionType=='checkbox'||newQuestion.questionType=='select'"
-      >
-        <div v-for="(qchoices,k) in newQuestion.questionChoices" :key="k">
-          <el-col :span="24" class="qChoice">
-            <el-input placeholder="Enter Question Choice" v-model="qchoices.choice">
+          <el-form-item
+            v-for="(field, index) in newQuestion.fields.choice "
+            :label="'Question Choice ' + (index+1)"
+            :key="field.key"
+            :prop="'fields.choice.' + index + '.data'"
+            :rules="{
+      required: true, message: 'Question Choice cannot be empty', trigger: 'blur'
+    }"
+          >
+            <el-input v-model="field.data" placeholder="Enter Question Choice">
               <el-button
                 slot="append"
                 icon="el-icon-minus"
-                @click="remove(k)"
-                v-show="k || (!k && newQuestion.questionChoices.length > 1 )"
+                @click="removeChoice(field)"
+                v-show="(index || (!index && newQuestion.fields.choice.length > 1 ))&&newQuestion.input_type!='switch'"
               ></el-button>
               <el-button
                 slot="append"
                 icon="el-icon-plus"
-                @click="add(k)"
-                v-show="k == newQuestion.questionChoices.length -1"
+                @click="addChoice()"
+                v-show="(index == newQuestion.fields.choice.length -1)&&newQuestion.input_type!='switch'"
               ></el-button>
             </el-input>
-          </el-col> 
+          </el-form-item>
         </div>
-      </el-form-item>
+        
 
-      <el-form-item label="Question Choice" v-if="newQuestion.questionType=='switch'">
-        <div v-for="(qchoices,k) in newQuestion.questionChoices" :key="k">
-          <el-col :span="24" class="qChoice">
-            <el-input placeholder="Enter Question Choice " v-model="qchoices.choice"></el-input>
+        <el-form-item label="Table Properties" v-if="newQuestion.input_type=='table'">
+          <div>
+            <el-col style="width:50%" class="qChoice">
+              <el-form-item label="Table Column">
+                <el-input-number
+                  placeholder="Enter Table Column"
+                  v-model.number.lazy="newQuestion.fields.column"
+                  :min="1"
+                  :max="10"
+                ></el-input-number>
+              </el-form-item>
+            </el-col>
+            <el-col style="width:50%" class="qChoice">
+              <el-form-item label="Table Row">
+                <el-input-number
+                  placeholder="Enter Table Row"
+                  v-model.number.lazy="newQuestion.fields.row"
+                  :min="1"
+                  :max="10"
+                ></el-input-number>
+              </el-form-item>
+            </el-col>
+            <el-form-item
+              v-for="(field, index) in newQuestion.fields.column "
+              :label="'Column Header ' + (index+1)"
+              :key="field.key"
+              :prop="'fields.choice.' + index + '.data'"
+              :rules="{
+      required: true, message: 'Column Header cannot be empty', trigger: 'blur'
+    }"
+            >
+              <el-input v-model="field.data" placeholder="Enter Question Choice"></el-input>
+            </el-form-item>
+
+          
+          </div>
+        </el-form-item>
+
+        <el-form-item label="Default Value">
+          <el-col :span="24">
+            <el-input placeholder="Enter Default Value" v-model="newQuestion.default_data"/>
           </el-col>
-        </div>
-      </el-form-item>
+        </el-form-item>
 
-      <el-form-item label="Table Properties" v-if="newQuestion.questionType=='table'">
-        <div>
-          <el-col :span="24" class="">
-            <el-input placeholder="Enter Table Column">
-          <el-col>
-        </div>
-      <el-form-item>
-
-      <el-form-item>
-        <el-col :span="24">
-          <el-switch v-model="newQuestion.is_hidden" active-text="Hidden"></el-switch>
-        </el-col>
-      </el-form-item>
-
-      <el-form-item>
-        <el-col :span="24">
-          <el-switch v-model="newQuestion.is_mandatory" active-text="Mandatory"></el-switch>
-        </el-col>
-      </el-form-item>
-      
-      <el-row>
-        <el-button type="primary" @click="createNewQuestion()">Save</el-button>
-        <el-button type="primary">Cancel</el-button>
-      </el-row>
-    </el-form>
+        <el-row>
+          <el-button type="primary" @click="submitForm('newQuestion')">Save</el-button>
+          <el-button type="primary" @click="navigateTo('index')">Cancel</el-button>
+        </el-row>
+      </el-form>
+    </el-card>
   </div>
 </template>
 
@@ -131,16 +174,18 @@ import { createQuestion } from "@/api/kyc/question";
 export default {
   data() {
     return {
+      rowCount: 0,
+      rows: [],
       categoryOption: [],
       typeOption: [],
       branches: [],
       sectionOption: [
         {
-          value: "b",
+          value: "B",
           label: "B"
         },
         {
-          value: "c",
+          value: "C",
           label: "C"
         }
       ],
@@ -177,29 +222,79 @@ export default {
       ],
 
       newQuestion: {
-        field_name:"",
-        display_text:"",
+        field_name: "",
+        display_text: "",
         section: "",
-        description:"",
-        origin:"entry",
+        description: "",
+        origin: "entry",
         category_id: "",
         type_id: "",
-        default_data:"",
-        only_default:true,
+        default_data: "",
+        only_default: true,
         input_type: "text",
-        fields: [],
+        fields: {
+          choice: [
+            {
+              data: "",
+              key: 1
+            }
+          ],
+          row: 0,
+          column: 0
+        },
         is_hidden: true,
         is_mandatory: true
-        
+      },
+      rules: {
+        display_text: [
+          {
+            required: true,
+            message: "Please input Question name",
+            trigger: "blur"
+          }
+        ],
+        section: [
+          {
+            required: true,
+            message: "Please select a Section",
+            trigger: "change"
+          }
+        ],
+        category_id: [
+          {
+            required: true,
+            message: "Please select a Category",
+            trigger: "change"
+          }
+        ],
+        type_id: [
+          { required: true, message: "Please select a Type", trigger: "change" }
+        ],
+        description: [
+          {
+            required: true,
+            message: "Please input Description",
+            trigger: "blur"
+          }
+        ],
+        choice: [
+          {
+            required: true,
+            message: "Please input Choice",
+            trigger: "blur"
+          }
+        ]
       }
     };
   },
   mounted() {
     this.getCategory();
     this.getType();
-   
   },
   methods: {
+    navigateTo(route) {
+      this.$router.push(route);
+    },
     async getCategory() {
       this.categoryOption = (await getCategory()).data.data;
     },
@@ -207,52 +302,86 @@ export default {
       this.typeOption = (await getType()).data.data;
     },
     async createNewQuestion() {
-      console.log(this.newUser)
-
+      console.log(this.newQuestion);
+      alert("submit!1");
+      alert(this.newQuestion);
       try {
-        await createQuestion(this.newQuestion)
-        console.log(this.newUser)
+        await createQuestion(this.newQuestion);
+        alert("submit!2");
+        this.$message(this.newQuestion.name + " " + "is registered");
+        this.newQuestion = "";
+
+        this.$router.push({
+          name: "index"
+        });
+        console.log(this.newQuestion);
       } catch (err) {
-        console.log(err)
+        alert("submit!3");
+        console.log(err);
+        alert(err);
       }
-
-      this.$message( this.newQuestion.name + ' ' +'is registered' )
-      this.newQuestion = ''
-
-      this.$router.push({
-        name: 'index'
-      })
     },
- 
+
     onChange() {
       if (
-        this.newQuestion.questionType == "radio" ||
-        this.newQuestion.questionType == "checkbox" ||
-        this.newQuestion.questionType == "select"
+        this.newQuestion.input_type == "radio" ||
+        this.newQuestion.input_type == "checkbox" ||
+        this.newQuestion.input_type == "select" ||
+        this.newQuestion.input_type == "table"
       ) {
-        this.newQuestion.questionChoices = [
+        this.newQuestion.fields.choice = [
           {
-            choice: ""
+            data: "",
+            key: 1
           }
         ];
-      } else if (this.newQuestion.questionType === "switch") {
-        this.newQuestion.questionChoices = [
+      } else if (this.newQuestion.input_type === "switch") {
+        this.newQuestion.fields.choice = [
           {
-            choice: ""
+            data: "",
+            key: 1
           },
           {
-            choice: ""
+            data: "",
+            key: 2
           }
         ];
       } else {
-        this.newQuestion.questionChoices = [];
+        this.newQuestion.fields.choice = [];
       }
     },
     add(index) {
-      this.newQuestion.questionChoices.push({ choice: "" });
+      this.newQuestion.fields.choice.push({ data: "" });
     },
     remove(index) {
-      this.newQuestion.questionChoices.splice(index, 1);
+      this.newQuestion.fields.choice.splice(index, 1);
+    },
+    removeChoice(item) {
+      var index = this.newQuestion.fields.choice.indexOf(item);
+      if (index !== -1) {
+        this.newQuestion.fields.choice.splice(index, 1);
+      }
+    },
+    addChoice() {
+      this.newQuestion.fields.choice.push({
+        key: Date.now(),
+        data: ""
+      });
+    },
+    addColumn(i) {
+      this.newQuestion.fields.choice.push({ data: "" });
+    },
+    submitForm(formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          this.newQuestion.field_name = this.newQuestion.display_text;
+          this.createNewQuestion();
+        } else {
+          console.log("error submit!!");
+          alert("The Form is not complete!");
+          return false;
+        }
+      });
     }
   }
 };
