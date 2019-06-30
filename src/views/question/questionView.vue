3b01acbfcdf8adb3ebb857c98e1b47c736896644
Lim Zi Hao <lzhao1880@gmail.com>
2019-07-01 00:32:54 +0800

    adjusted margin, fixed table input

--- src/views/question/questionView.vue
+++ src/views/question/questionView.vue
@@ -1,28 +1,32 @@
 <template>
   <div class="app-container">
     <el-row>
-      <el-col span="18">
-        <el-card class="box-card">
-          <el-row>
-            <el-button
-              icon="el-icon-back"
-              v-if="viewMode==true"
-              v-on:click="navigateBack()"
-              style="float:left"
-            >Back</el-button>
-            <el-button
-              icon="el-icon-delete"
-              v-if="viewMode==true"
-              v-on:click="deleteConfirmation()"
-              style="float:right"
-            >Delete</el-button>
-            <el-button
-              icon="el-icon-edit"
-              v-if="viewMode==true"
-              v-on:click="makeEditable()"
-              style="float:right"
-            >Edit</el-button>
-          </el-row>
+      <div style="margin: 0px 100px 45px 100px">
+        <el-button
+          type="primary"
+          icon="el-icon-back"
+          v-on:click="navigateBack()"
+          style="float:left"
+        >Back</el-button>
+        <el-button
+          type="primary"
+          icon="el-icon-delete"
+          v-if="viewMode==true"
+          v-on:click="deleteConfirmation()"
+          style="float:right"
+        >Delete</el-button>
+        <el-button
+          type="primary"
+          icon="el-icon-edit"
+          v-if="viewMode==true"
+          v-on:click="makeEditable()"
+          style="float:right"
+        >Edit</el-button>
+      </div>
+    </el-row>
+    <el-row>
+      <el-col>
+        <el-card class="box-card" style="margin: 10px 100px">
           <el-form
             label-position="top"
             :rules="rules"
@@ -116,7 +120,7 @@
               </el-col>
               <el-col style="width:33%">
                 <el-form-item label="Hidden Question">
-                  <el-switch v-model="question.is_hidden" active-text="Hidden"></el-switch>
+                  <el-switch v-model="question.is_hidden" active-text="Hidden" ></el-switch>
                 </el-form-item>
               </el-col>
               <el-col style="width:33%">
@@ -125,7 +129,7 @@
                 </el-form-item>
               </el-col>
             </el-row>
-            <div
+            <div 
               v-if="question.input_type=='radio'||question.input_type=='checkbox'||question.input_type=='select'||question.input_type=='switch'"
             >
               <el-form-item
@@ -137,7 +141,7 @@
       required: true, message: 'Question Choice cannot be empty', trigger: 'blur'
     }"
               >
-                <el-input v-model="field.data" placeholder="Enter Question Choice">
+                <el-input v-model="field.data" placeholder="Enter Question Choice" >
                   <el-button
                     slot="append"
                     icon="el-icon-minus"
@@ -155,37 +159,38 @@
             </div>
 
             <el-form-item label="Table Properties" v-if="question.input_type=='table'">
-              <div>
-                <el-col style="width:50%" class="qChoice">
-                  <el-form-item label="Table Column">
-                    <el-input-number
-                      placeholder="Enter Table Column"
-                      v-model.number.lazy="question.fields.column"
-                      :min="1"
-                    ></el-input-number>
-                  </el-form-item>
-                </el-col>
-                <el-col style="width:50%" class="qChoice">
-                  <el-form-item label="Table Row">
-                    <el-input-number
-                      placeholder="Enter Table Row"
-                      v-model.number.lazy="question.fields.row"
-                      :min="1"
-                    ></el-input-number>
-                  </el-form-item>
-                </el-col>
-                <el-form-item
-                  v-for="(field, index) in question.fields.column "
-                  :label="'Column Header ' + (index+1)"
-                  :key="field.key"
-                  :prop="'fields.choice.' + index + '.data'"
-                  :rules="{
+              <el-form-item
+                v-for="(field, index) in question.fields.choice "
+                :label="'Column Header ' + (index+1)"
+                :key="field.key"
+                :prop="'fields.choice.' + index + '.data'"
+                :rules="{
       required: true, message: 'Column Header cannot be empty', trigger: 'blur'
     }"
-                >
-                  <el-input v-model="field.data" placeholder="Enter Question Choice"></el-input>
-                </el-form-item>
-              </div>
+              >
+                <el-input v-model="field.data" placeholder="Enter Column Header">
+                  <el-button
+                    slot="append"
+                    icon="el-icon-minus"
+                    @click="removeChoice(field)"
+                    v-show="(index || (!index && question.fields.choice.length > 1 ))&&question.input_type!='switch'"
+                  ></el-button>
+                  <el-button
+                    slot="append"
+                    icon="el-icon-plus"
+                    @click="addChoice()"
+                    v-show="(index == question.fields.choice.length -1)&&question.input_type!='switch'"
+                  ></el-button>
+                </el-input>
+              </el-form-item>
+
+              <el-form-item label="Table Rows">
+                <el-input-number
+                  placeholder="Enter Table Row"
+                  v-model.number.lazy="question.fields.row"
+                  :min="1"
+                ></el-input-number>
+              </el-form-item>
             </el-form-item>
 
             <el-form-item label="Default Value (Optional)">
@@ -202,10 +207,7 @@
         </el-card>
       </el-col>
     </el-row>
-    <el-dialog
-      :visible.sync="dialogDelete"
-      title="Delete Confirmation"
-      width="30%">
+    <el-dialog :visible.sync="dialogDelete" title="Delete Confirmation" width="30%">
       <span>Delete this Question?</span>
       <span slot="footer" class="dialog-footer">
         <el-button @click="dialogDelete = false">Cancel</el-button>
@@ -216,7 +218,11 @@
 </template>
 
 <script>
-import { getQuestionShow, putQuestion , deleteQuestion } from "@/api/kyc/question";
+import {
+  getQuestionShow,
+  putQuestion,
+  deleteQuestion
+} from "@/api/kyc/question";
 import { getCategory } from "@/api/kyc/category";
 import { getType } from "@/api/kyc/type";
 import Pagination from "@/components/Pagination";
@@ -227,7 +233,8 @@ export default {
   },
   data() {
     return {
-      dialogDelete:false,
+      jsonstring: "",
+      dialogDelete: false,
       viewMode: true,
       categoryOption: [],
       typeOption: [],
@@ -272,7 +279,8 @@ export default {
         }
       ],
       question: {
-        field_name: "aa",
+        id: "",
+        field_name: "",
         display_text: "",
         section: "",
         description: "",
@@ -283,12 +291,61 @@ export default {
         only_default: true,
         input_type: "",
         fields: {
-          choice: [],
+          choice: [
+            {
+              data: "",
+              key: ""
+            }
+          ],
           column: 0,
           row: 0
         },
-        is_hidden: true,
+        is_hidden: false,
         is_mandatory: true
+      },
+      rules: {
+        display_text: [
+          {
+            required: true,
+            message: "Please Enter Question Name",
+            trigger: "blur"
+          }
+        ],
+        section: [
+          {
+            required: true,
+            message: "Please Enter a Section",
+            trigger: "change"
+          }
+        ],
+        category_id: [
+          {
+            required: true,
+            message: "Please Select Question Category",
+            trigger: "change"
+          }
+        ],
+        type_id: [
+          {
+            required: true,
+            message: "Please Select Question Type",
+            trigger: "change"
+          }
+        ],
+        description: [
+          {
+            required: true,
+            message: "Please Enter Question Description",
+            trigger: "blur"
+          }
+        ],
+        choice: [
+          {
+            required: true,
+            message: "Please Enter Question Choice",
+            trigger: "blur"
+          }
+        ]
       }
     };
   },
@@ -311,22 +368,22 @@ export default {
       this.typeOption = (await getType()).data.data;
     },
     async getQuestion() {
-      const questionId = this.$store.state.route.params.questionId;
+      this.question.id = this.$store.state.route.params.questionId;
 
-      getQuestionShow(questionId)
+      getQuestionShow(this.question.id)
         .then(res => {
           const data = res.data.data;
           (this.question.field_name = data.field_name),
             (this.question.display_text = data.display_text),
             (this.question.section = data.section),
             (this.question.description = data.description),
-            (this.question.origin = data.origin);
-          (this.question.category_id = data.category.id),
+            (this.question.origin = data.origin),
+            (this.question.category_id = data.category.id),
             (this.question.type_id = data.type.id),
             (this.question.default_data = data.default_data),
             (this.question.only_default = data.only_default),
             (this.question.input_type = data.input_type),
-            (this.question.fields = data.fields),
+            (this.question.fields = JSON.parse(data.fields)),
             (this.question.is_hidden = data.is_hidden),
             (this.question.is_mandatory = data.is_mandatory);
         })
@@ -338,7 +395,8 @@ export default {
     },
     async updateQuestion() {
       console.log(this.question);
-
+      this.question.field_name = this.question.display_text;
+      this.question.fields = JSON.stringify(this.question.fields);
       try {
         await putQuestion(this.question);
         console.log(this.question);
@@ -347,25 +405,25 @@ export default {
           message: this.question.display_text + " field " + "is updated",
           type: "success"
         });
-
+        this.question.fields = JSON.parse(this.question.fields);
         this.viewMode = true;
       } catch (err) {
         console.log(err);
       }
     },
     async deleteQuestion() {
-      console.log()
+      console.log();
       try {
-        await deleteQuestion(this.$store.state.route.params.questionId)
+        await deleteQuestion(this.question.id);
       } catch (err) {
-        console.log(err)
-        alert(err)
+        console.log(err);
+        alert(err);
       }
-      this.dialogDelete = false
-      this.navigateBack()
+      this.dialogDelete = false;
+      this.navigateBack();
     },
     async deleteConfirmation() {
-      this.dialogDelete = true
+      this.dialogDelete = true;
     },
     makeEditable() {
       this.viewMode = false;
@@ -421,9 +479,9 @@ export default {
 
 <style>
 .qChoice {
-  margin-bottom: 20px;
+  margin-bottom: 30px;
   &:last-child {
     margin-bottom: 0;
   }
-} 
+}
 </style>
