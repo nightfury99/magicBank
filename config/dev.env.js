e45ec641861942ca1edb66f8ede0c9e6ed44475c
Close Loop <balainkk@gmail.com>
2019-07-23 00:47:23 +0800

    sync merge

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,9 +4,9 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_API: '"http://13.229.122.17/api"',
-  FILE_API: '"http://52.221.193.133:4000"',
-  MEDIA_PATH: '"http://52.221.193.133"',
+  BASE_API: '"http://54.169.173.84/api"',
+  FILE_API: '"http://54.169.173.84:4000"',
+  MEDIA_PATH: '"http://54.169.173.84"',
   STORAGE_FOLDER: '"gallery"',
   MARKER_FOLDER: '"marker"',
 })
